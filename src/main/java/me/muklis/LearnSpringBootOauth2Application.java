638752db50a38b33948592b2c9ee40c73af3e2a0
monggopesen <muhashari64@gmail.com>
2020-03-22 09:57:34 +0700

    Initial Commit

---
+++ src/main/java/me/muklis/LearnSpringBootOauth2Application.java
@@ -0,0 +1,15 @@
+package me.muklis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@EnableAuthorizationServer
+@SpringBootApplication
+public class LearnSpringBootOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/muklis/LearnSpringBootOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.muklis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
